383d5cc647b50b487b5c520e7195e97837d1b68b
delaylee <delaylee219@gmail.com>
2022-12-11 11:25:53 +0900

    test(springboot): HelloController hello 테스트 구현

---
+++ src/test/java/com/graydelay/book/springboot/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.graydelay.book.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
