5da1a9bb4c52618ab45351a91cb3a5a0e6103368
delaylee <delaylee219@gmail.com>
2022-12-12 23:26:50 +0900

    test: posts 수정 동작 확인 테스트 구현

--- src/main/java/com/graydelay/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/graydelay/book/springboot/service/posts/PostsService.java
@@ -7,6 +7,7 @@ import com.graydelay.book.springboot.web.dto.PostsSaveRequestDto;
 import com.graydelay.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +19,7 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
