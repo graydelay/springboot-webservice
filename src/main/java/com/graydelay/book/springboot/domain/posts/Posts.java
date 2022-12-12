6effe4e96ee4534d88fd3d0665189c3a032ea070
delaylee <delaylee219@gmail.com>
2022-12-12 22:47:51 +0900

    feat: Posts 수정(update), 조회(findById) 기능 구현

--- src/main/java/com/graydelay/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/graydelay/book/springboot/domain/posts/Posts.java
@@ -29,4 +29,10 @@ public class Posts {
         this.content = content;
         this.author = author;
     }
+
+    //==비지니스 로직==//
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
--- src/main/java/com/graydelay/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/graydelay/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.graydelay.book.springboot.service.posts;
 
+import com.graydelay.book.springboot.domain.posts.Posts;
 import com.graydelay.book.springboot.domain.posts.PostsRepository;
+import com.graydelay.book.springboot.web.dto.PostsResponseDto;
 import com.graydelay.book.springboot.web.dto.PostsSaveRequestDto;
+import com.graydelay.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +16,21 @@ public class PostsService {
 
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
