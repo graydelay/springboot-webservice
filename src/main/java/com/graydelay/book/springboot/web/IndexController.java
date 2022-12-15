fa44e78dc5a2619267a6214a9272b17087fab6a2
delaylee <delaylee219@gmail.com>
2022-12-15 20:16:10 +0900

    feat: 게시글 전체 조회 api, 화면 구현

--- src/main/java/com/graydelay/book/springboot/web/IndexController.java
+++ src/main/java/com/graydelay/book/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.graydelay.book.springboot.web;
 
+import com.graydelay.book.springboot.service.posts.PostsService;
+import com.graydelay.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
